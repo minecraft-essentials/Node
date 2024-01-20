ed849cf4b253be7d423a44a13c01ffa3605dc102
eveeify <eveeg1971@gmail.com>
2024-01-20 21:58:11 +1100

    feat: npm bindings

--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,52 @@ extern crate napi_derive;
 pub fn sum(a: i32, b: i32) -> i32 {
   a + b
 }
+
+#[napi]
+pub struct Oauth {
+  client_id: String,
+}
+
+#[napi]
+pub struct DeviceCode {
+    client_id: String,
+}
+
+#[napi]
+impl Oauth {
+  #[napi(constructor)]
+  pub fn new(client_id: String) -> Self {
+      Self { client_id }
+  }
+
+  #[napi]
+  pub fn url(&self) -> String {
+    self.client_id.clone()
+  }
+
+  #[napi]
+  pub fn launch() -> String {
+    "launch called".to_string()
+  }
+}
+
+
+
+#[napi]
+impl DeviceCode {
+  #[napi(constructor)]
+  pub fn new(client_id: String) -> Self {
+      Self { client_id }
+  }
+
+  #[napi]
+  pub fn prelaunch(&self) -> String {
+    self.client_id.clone()
+  }
+
+  #[napi]
+  pub fn launch() -> String {
+    "Device launch called".to_string()
+  }
+}
+
