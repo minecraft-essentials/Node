cb08293ee223e091c69e9c38210b544a1fbe6f62
eveeify <eveeg1971@gmail.com>
2024-02-08 21:26:33 +1100

    "feat: git backup"

--- src/lib.rs
+++ src/lib.rs
@@ -1,54 +1,91 @@
 #![deny(clippy::all)]
-#![forbid(unsafe_code)]
+
+use minecraft_essentials::Oauth as MinecraftOauth;
+use minecraft_essentials::AuthData;
+use napi::bindgen_prelude;
 
 #[macro_use]
 extern crate napi_derive;
 
-#[napi]
-pub fn sum(a: i32, b: i32) -> i32 {
-  a + b
-}
+
 
 #[napi]
 pub struct Oauth {
-  client_id: String,
+    oauth: minecraft_essentials::Oauth,
+}
+
+
+use napi::{CallContext, Env, JsObject, JsString, JsUnknown, Result};
+use napi_derive::module_exports;
+
+pub struct AuthDataWrapper {
+    inner: minecraft_essentials::AuthData,
+}
+
+impl AuthDataWrapper {
+    pub fn new(inner: minecraft_essentials::AuthData) -> Self {
+        Self { inner }
+    }
+}
+
+#[napi]
+impl AuthDataWrapper {
+    #[napi(getter)]
+    pub fn get_access_token(&self, ctx: CallContext) -> Result<JsString> {
+        let env = ctx.env;
+        let access_token = self.inner.access_token; // Access the access_token field directly
+        env.create_string(&access_token)
+    }
+
+    // Implement other getters or methods as needed to expose the necessary data
+}
+
+
+
+#[napi]
+impl Oauth {
+ #[napi(constructor)]
+ pub async fn new(client_id: String, port: u16) -> Self {
+    let oauth = minecraft_essentials::Oauth::new(client_id.as_str(), Some(port));
+    Self { oauth }
+ }
+
+ #[napi]
+ pub fn url(&self) -> &str {
+    &self.oauth.url()
+ }
+
+ #[napi]
+ pub async fn launch(&self, client_secret: String, bedrockRel: bool) -> Result<AuthData, Box<dyn std::error::Error>> {
+     let oauth_info = self.oauth.launch(bedrockRel, &client_secret).await.map_err(|e| {
+         // Convert the error `e` into the desired error type here
+         // For example, if `e` is a custom error type, you might do something like this:
+         Err(Some(format!("{}", e)))
+     })?;
+     Ok(oauth_info)
+ }
+
 }
 
 #[napi]
 pub struct DeviceCode {
-    client_id: String,
+  url: String,
 }
-
-#[napi]
-impl Oauth {
-  #[napi(constructor)]
-  pub fn new(client_id: String) -> Self {
-      Self { client_id }
-  }
-
-  #[napi]
-  pub fn url(&self) -> String {
-    self.client_id.clone()
-  }
-
-  #[napi]
-  pub fn launch() -> String {
-    "launch called".to_string()
-  }
-}
-
-
 
 #[napi]
 impl DeviceCode {
   #[napi(constructor)]
-  pub fn new(client_id: String) -> Self {
-      Self { client_id }
+  pub fn new(client_id: String, port: u16 ) -> Self {
+    println!("\x1b[33mNOTICE: You are using and Experiemntal Feature.\x1b[0m");
+
+    let url = format!("fjnbdsjkhfbsdgfbejsd");
+
+    Self { url }
   }
 
   #[napi]
   pub fn prelaunch(&self) -> String {
-    self.client_id.clone()
+    self.url.clone()
   }
 
   #[napi]
